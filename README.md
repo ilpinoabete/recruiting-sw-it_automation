733f6cd25ebf2a828546e6d5db6541920c463350
ilpinoabete <cristianotollot@gmail.com>
2025-09-12 15:47:24 +0200

    Rpository recreated and added README.md, bot-telegram.py and docker-compose.yaml

---
+++ README.md
@@ -0,0 +1,30 @@
+# Introduction to the assignment
+
+Given the assigned tasks i decided to proceed with the first one, whose instructions asked me to ssh into a given ubuntu server VM, install docker server on it and spin up a NocoDB container.
+After that i was requested to create a python script that was able to receive a JSON stream and parse and push them into the database thanks to the REST APIs. It was also requested to create a Telegram BOT that was able to retrieve certain data and add new user to the hosted database.
+
+# Docker and database setup
+## Docker installation
+First of all i made sure the server was up to date running the command `sudo apt update && sudo apt upgrade -y` I than installed the editor `micro` to edit the configuration files.
+After that i could install docker trough the official script available in the [official documentation](https://docs.docker.com/engine/install/ubuntu/) that i downloaded with ``curl -fsSL https://get.docker.com -o get-docker.sh`` and executed launching the follow command `sudo chmod +x get-docker.sh && sudo ./get-docker.sh`.
+
+After verifying that docker was actually installed in the system and doing the same for python i could carry on with the task
+```bash
+user@recruiting-hr1:~$ docker -v
+Docker version 28.4.0, build d8eb465
+
+user@recruiting-hr1:~$ which python3
+/usr/bin/python3
+```
+
+## NocoDB installation
+The second task in order of business was to install NocoDB in the VM, to do so i decided to use docker compose as to as make the maintenance of the future whole application stack easier.
+Following the [official NocoDB docker cocumentation](https://nocodb.com/docs/self-hosting/installation/docker) i pulled the official NocoDB repository via the command `
+`git clone https://github.com/nocodb/nocodb` i than moved to the `nocodb/docker-compose/2_pg` directory and i slightly modified the default compose with these goals:
+* Remove postgres sensible information (user, password, and database name) frome the file to use a .env
+* Create a custom network to make it easier to group the stack's container
+
+The `docker-compose.yaml` as well as the `.env` files can be found in this repository as all the other files whose upload was requested by the task.
+
+Once the setup was completed i could run the `sudo docker compose up -d` command and, as expected, the NocoDB webUI was reachable at the `http://serverLocalIP:8080` address.
+I successfully logged into the NocoDB webUI and them I could create the Users, Departments and Areas tables and started working on the two python scripts.
---
+++ bot-telegram.py
@@ -0,0 +1,127 @@
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from dotenv import load_dotenv
+from io import StringIO
+import requests as r
+import csv
+import os
+
+load_dotenv()
+
+# List of allowed users to use the bot
+ALLOWED_USERS = eval(os.getenv("ALLOWED_USERS"))
+# Authentication header for NocoDB API
+AUTH_HEADER = {"xc-token": os.getenv("DB_KEY")}
+
+def generate_csv(data: list[dict]) -> StringIO:
+    """
+    Converts a list of dictionaries to a CSV file stored in memory (StringIO).
+    Returns the in-memory file object.
+
+    Args:
+        data (list[dict]): List of dictionaries.
+
+    Returns:
+        StringIO: CSV file.
+    """
+
+    # Basic validation
+    if not data:
+        raise ValueError("No data provided to generate CSV.")
+
+    # Create in-memory CSV file
+    output = StringIO()
+    writer = csv.DictWriter(output, fieldnames=data[0].keys())
+    writer.writeheader()
+    writer.writerows(data)
+    output.seek(0) # Move to the beginning of the StringIO object
+
+    return output
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Send a message when the command /start is issued.
+    """
+
+    await update.message.reply_text("Hi, I am an useful bot that can retrieve data from your NocoDB installation!\n"
+                                    "The command that i support is /list, to list the items of a certain table"
+                                    )
+
+async def list_tables(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Retrieves the available tables from the NocoDB installation and lets the user select which one to show the information of.
+    """
+
+    # Check if the user is allowed to use the bot
+    if update.message.chat.username in ALLOWED_USERS:
+
+        # Retrieve the list of existing tables
+        tables = r.get(
+                   f"http://{os.getenv('SERVER_URL')}/api/v2/meta/bases/{os.getenv('BASE_ID')}/tables",
+                   headers=AUTH_HEADER,
+                ).json()
+
+        tables = tables["list"]
+        keyboard = []
+
+        # Crating the keyboard with the available tables
+        for i in range(len(tables)):
+            keyboard.append([InlineKeyboardButton(tables[i]['name'], callback_data=tables[i]['id'])])
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        # Asking the user which table to show
+        await update.message.reply_text("Which table do you want to list the content of?", reply_markup=reply_markup)
+
+    else:
+        await update.message.reply_text("Sorry, you are not allowed to use this command.")
+
+async def list_items(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Receives the selected table id from the `list_tables` function and retrieves its content.
+    Then it calls the `generate_csv` function and returns the CSV file to the user
+    """
+
+    # Acknowledge the callback query
+    query = update.callback_query
+    await query.answer()
+
+    # Retrieve the selected table content
+    table_content = r.get(
+        f"http://{os.getenv('SERVER_URL')}/api/v2/tables/{query.data}/records",
+        headers=AUTH_HEADER
+    ).json()
+
+    table_content = table_content["list"]
+    csv_file = generate_csv(table_content)
+
+    # Sending the CSV file to the user
+    await context.bot.send_document(
+        chat_id=update.effective_chat.id,
+        document=csv_file,
+        filename="members.csv"
+    )
+
+    del csv_file
+
+def main() -> None:
+    # Create the bot with the environment API key
+    bot = Application.builder().token(os.getenv("TELEGRAM_API_KEY")).build()
+
+    # Adding handlers
+    bot.add_handler(CommandHandler("start", start))
+    bot.add_handler(CommandHandler("list", list_tables))
+    bot.add_handler(CallbackQueryHandler(list_items))
+
+    # Keep the bot running
+    bot.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except KeyboardInterrupt:
+        exit(0)
+
+    except Exception as e:
+        print(e)
